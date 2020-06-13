f2f66d74ce95c5d2e354db2483f9eff06249f687
webCtwo <2361470365@qq.com>
2020-06-13 11:07:05 +0800

    初始化提交

---
+++ src/main/java/com/test/TestController.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @描述TODO:
+ * @author: Mr.Cui
+ * @create: 2020-06-13 10:58
+ **/
+@RestController
+public class TestController {
+
+    @GetMapping("get")
+    public String test() {
+        return "hello world";
+    }
+
+}
